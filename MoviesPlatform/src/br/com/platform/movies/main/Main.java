7de9cab4994ad8e98eb9f35a3c331803ed227a6a
Elieudo Maia <elieudo.maia@gmail.com>
2021-04-20 06:48:39 -0300

    atualizar filme

--- MoviesPlatform/src/br/com/platform/movies/main/Main.java
+++ MoviesPlatform/src/br/com/platform/movies/main/Main.java
@@ -7,6 +7,8 @@ import br.com.platform.movies.view.Home;
 import java.text.ParseException;
 import java.sql.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +21,15 @@ public class Main {
       new Home().setVisible(true);
     
 //    MovieController mc = new MovieController();
-//    
+    
+    // Atualizar filme
+//    try {
+//      mc.updateMovie(1, "Editado", "Comédia", "Filme de comédia", 120, true, 12);
+//      System.out.println("Filme editado com sucesso");
+//    } catch (Exception ex) {
+//      System.out.println(ex.getMessage());
+//    }
+
 //    //criando filme
 //    mc.createMovie("Hora de dormir", "Humor", "Filme de humor", 130, true, 14);
 //
@@ -39,18 +49,18 @@ public class Main {
 //    } catch (Exception ex) {
 //      System.out.println(ex.getMessage());
 //    }
-//    
+////    
 //    if (m instanceof Movie) {
 //      System.out.println("&&&&&&&" + m.getName());
 //    }
 
-      PersonController pc = new PersonController(); 
-      Date birth = new Date(1997, 9, 18);
-      try {
-        pc.create("Elieudo Maia", "elieudo.maia@gmail.com", "123", "88998758585", "Em casa", birth);
-      } catch (Exception ex) {
-        System.out.println(ex.getMessage());
-      }
-
+//      PersonController pc = new PersonController(); 
+//      Date birth = new Date(1997, 9, 18);
+//      try {
+//        pc.create("Elieudo Maia", "elieudo.maia@gmail.com", "123", "88998758585", "Em casa", birth);
+//      } catch (Exception ex) {
+//        System.out.println(ex.getMessage());
+//      }
+    
   }
 }
